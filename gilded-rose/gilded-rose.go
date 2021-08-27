95ebf43a49b18b1ecf0199a46a9cb4aa77ec0b53
sovernut <orenomail4@gmail.com>
2021-08-28 03:27:01 +0700

    refactor

--- gilded-rose/gilded-rose.go
+++ gilded-rose/gilded-rose.go
@@ -5,53 +5,61 @@ type Item struct {
 	sellIn, quality int
 }
 
+func isIn(name string, items []string) bool {
+	for _, item := range items {
+		if name == item {
+			return true
+		}
+	}
+	return false
+}
+
+func increaseQuality(item *Item) {
+	if item.quality < 50 {
+		item.quality++
+	}
+}
+
+func decreaseQuality(item *Item) {
+	if item.quality > 0 {
+		item.quality--
+	}
+}
+
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
-			}
+		if isIn(items[i].name, []string{"Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"}) {
+			increaseQuality(items[i])
 		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
+			decreaseQuality(items[i])
+		}
+
+		if items[i].name == "Backstage passes to a TAFKAL80ETC concert" && items[i].quality < 50 {
+			if items[i].sellIn < 11 {
+				items[i].quality++
+			}
+			if items[i].sellIn < 6 {
+				items[i].quality++
 			}
 		}
 
 		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
+			items[i].sellIn--
 		}
 
 		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
-					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
-				}
-			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
-				}
+			if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+				items[i].quality = 0
+				continue
 			}
+
+			if isIn(items[i].name, []string{"Aged Brie", "Sulfuras, Hand of Ragnaros"}) {
+				increaseQuality(items[i])
+				continue
+			}
+
+			decreaseQuality(items[i])
 		}
 	}
 
